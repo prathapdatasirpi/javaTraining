1474a0e214cd59504ac2cc76ca27b7edd6a05f5d
SPrathaban <sprathaban@kingpin.global>
2023-05-26 12:37:36 +0530

    Java conditions

--- src/main/java/com/test/javaTraining/JavaTrainingApplication.java
+++ src/main/java/com/test/javaTraining/JavaTrainingApplication.java
@@ -6,7 +6,52 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaTrainingApplication {
 
+	static void javaConditions() {
+				// Java  variables
+				String name = "Prathap";
+				int rollNo = 12;
+				boolean isEmployee = true;
+				double doubleValue = 1.43;
+				System.out.println("String Type " + name);
+				System.out.println("Integer Type " + rollNo);
+				System.out.println("Double Type " + doubleValue);
+				System.out.println("Boolean Type " + isEmployee);
+		
+				// Java conditions
+		
+				if(isEmployee) {
+					System.out.println(name + " is a Employee");
+				} else {
+					System.out.println(name + " is not a Employee");
+				}
+		
+				// for loop 
+				int i;
+				System.out.println("Print 0 to 9 in for loop");
+				for( i = 0; i < 10 ;i++) {
+					System.out.print(i + " ");
+		
+				}
+				// while loop 
+				System.out.println("\nPrint 0 to 9 in while loop");
+				i = 0;
+				while(i < 10) {
+					System.out.print(i + " ");
+					i++;
+		
+				}
+				System.out.println("\nPrint 0 to 9 in do while loop");
+				i = 0;
+				do {
+					System.out.print(i + " ");
+					i++;
+		
+				} while(i < 10);
+
+	}
+
 	public static void main(String[] args) {
+		javaConditions();
 		SpringApplication.run(JavaTrainingApplication.class, args);
 	}
 
