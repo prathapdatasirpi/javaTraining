130422a9dbad02d7124188761961a1794d12c098
SPrathaban <sprathaban@kingpin.global>
2023-05-26 10:47:44 +0530

    first commit

---
+++ src/main/java/com/test/javaTraining/JavaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.test.javaTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/javaTraining/JavaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.javaTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
