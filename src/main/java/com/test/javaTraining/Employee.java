6b6fcb653997abc3e49295d85bdefd060cf808d1
SPrathaban <sprathaban@kingpin.global>
2023-06-05 08:54:12 +0530

    Java methods, access specifiers and comments

--- src/main/java/com/test/javaTraining/Employee.java
+++ src/main/java/com/test/javaTraining/Employee.java
@@ -4,14 +4,29 @@ public class Employee {
     // Creating instance variable
     String name;
     int empId;
-    // Creating constructor for Employee with name and empId
-    Employee(String name, int empId) {
+    long salary;
+    // Creating constructor for Employee with name, empId and salary
+    Employee(String name, int empId, long salary) {
         this.name = name;
         this.empId = empId;
-        System.out.println("Assigned Employee name : " + name + " and Id :" + empId + " to the class variables using constructor");
+        this.salary = salary;
     }
-    void printEmployeeDetails() {
+    // Declaring a public method/Function in class
+    public void printEmployeeDetails() {
         System.out.println("Employee name : " + this.name);
-        System.out.println("Employee Id : " + this.empId);
+        System.out.println("Employee Id : " + this.empId); 
+        //calling private method accessible only within class
+        this.printEmployeeSalary();
+    }
+    //Declaring private method
+    private void printEmployeeSalary() {
+        System.out.println("Employee salary in private method: " + this.salary);
+        // Calling protected method within class
+        this.assignSalary(50000);
+    }
+    // Declaring protected method
+    protected void assignSalary(long salary) {
+        this.salary = salary;
+        System.out.println("Employee salary in protected method : " + this.salary);
     }
 }
--- src/main/java/com/test/javaTraining/JavaTrainingApplication.java
+++ src/main/java/com/test/javaTraining/JavaTrainingApplication.java
@@ -5,63 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaTrainingApplication {
-	//Declare static variable
-	public static String JAVA_TRAINING = "JAVA_TRAINING";
-
-	//Declare static method
-	static void javaConditions() {
-				// Java  variables
-				String name = "Prathap";
-				int rollNo = 12;
-				boolean isEmployee = true;
-				double doubleValue = 1.43;
-				System.out.println("String Type " + name);
-				System.out.println("Integer Type " + rollNo);
-				System.out.println("Double Type " + doubleValue);
-				System.out.println("Boolean Type " + isEmployee);
-		
-				// Java conditions
-		
-				if(isEmployee) {
-					System.out.println(name + " is a Employee");
-				} else {
-					System.out.println(name + " is not a Employee");
-				}
-		
-				// for loop 
-				int i;
-				System.out.println("Print 0 to 9 in for loop");
-				for( i = 0; i < 10 ;i++) {
-					System.out.print(i + " ");
-		
-				}
-				// while loop 
-				System.out.println("\nPrint 0 to 9 in while loop");
-				i = 0;
-				while(i < 10) {
-					System.out.print(i + " ");
-					i++;
-		
-				}
-				System.out.println("\nPrint 0 to 9 in do while loop");
-				i = 0;
-				do {
-					System.out.print(i + " ");
-					i++;
-		
-				} while(i < 10);
-
-	}
 
 	public static void main(String[] args) {
-		// Calling Static method
-		javaConditions();
 		// Create object for Employee class and passed values via paramteres for
-		Employee emp = new Employee("Prathap", 0123);
+		Employee emp = new Employee("Prathap", 0123, 10000);
+
+		// calling public method
 		emp.printEmployeeDetails();
-		System.out.println("Printing Static variable " + JAVA_TRAINING);
-		
+
+		// Calling protected method outside class within package
+		emp.assignSalary(1000000);
 		SpringApplication.run(JavaTrainingApplication.class, args);
 	}
+
+	// Single line comment
+	/*
+	 * Multiple line comment
+	 */
 
 }
